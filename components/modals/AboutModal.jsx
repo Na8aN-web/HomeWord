d0ad333e23f7ed2e5ebca7c4d07a002e4c4ada8c
Nathan Kaduru <nathan.kaduru@flexisaf.com>
2023-09-07 00:00:22 +0100

    new

--- components/modals/AboutModal.jsx
+++ components/modals/AboutModal.jsx
@@ -26,7 +26,7 @@ const AboutModal = ({ isOpen, onClose }) => {
     isOpen && (
       <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center z-50 p-8">
         <div ref={modalRef} className="bg-gray-800 p-4 w-3/4 md:w-1/2 h-96 md:h-auto rounded-md overflow-auto">
-          <h2 className="text-lg text-white mb-2 font-mont">Hi i'm Nathan</h2>
+          <h2 className="text-lg text-white mb-2 font-mont">Hi i&apos;m Nathan</h2>
           <p className="text-sm font-mont text-white">
             This is a personal project Bible website I built using Next, Tailwind
             and Firebase, it includes features such as authorization, reading
@@ -35,7 +35,7 @@ const AboutModal = ({ isOpen, onClose }) => {
           </p>
           <p className="text-sm font-mont text-white">
             If you have any questions, feel free to reach out to me at{" "}
-            <a href="mailto:oblivin008@gmail.com">oblivin008@gmail.com</a>, and if you'd like to take a look at more of my
+            <a href="mailto:oblivin008@gmail.com">oblivin008@gmail.com</a>, and if you&apos;d like to take a look at more of my
             projects, refer to my portfolio link below.
           </p>
           <Link
