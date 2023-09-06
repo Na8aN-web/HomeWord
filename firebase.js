cb2fc9cb02fdb9d052220dc025d8d3ef615cbd3d
Nathan Kaduru <nathan.kaduru@flexisaf.com>
2023-09-06 03:42:05 +0100

    hi

--- firebase.js
+++ firebase.js
@@ -1,19 +1,18 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getApps, getApp } from "firebase/app";
-import "firebase/database"
+import "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBLHD9sCFV_WgfKdcNK8gJLeYU1dG79B0M",
-  authDomain: "readn-8c472.firebaseapp.com",
-  databaseURL: "https://readn-8c472-default-rtdb.firebaseio.com",
-  projectId: "readn-8c472",
-  storageBucket: "readn-8c472.appspot.com",
-  messagingSenderId: "1049182835415",
-  appId: "1:1049182835415:web:af75bbcaed84cdf24a1692",
-  measurementId: "G-FMNQ26GYD9"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
 };
-
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 export const auth = getAuth(app);
